286030ec572dd181d17e4481a79c459527a1d5f5
saikb92 <43761050+saikb92@users.noreply.github.com>
2019-03-30 01:10:41 -0400

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,13 @@
+def sum(a,b):
+    return a+b
+
+def avg(a,b):
+    return sum(a,b)//2
+
+a= float(input("Enter first number:"))
+b= float(input("Enter second number:"))
+c= input("enter the test")
+
+print("Sum of the given two numbers is: ", sum(a,b))
+print("Average of the given numbers is: ", avg(a,b))
+print("Input c:", c)
